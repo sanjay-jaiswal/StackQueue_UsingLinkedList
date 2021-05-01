11fffed709eaf201ec4caa5fe30bfbbea8061bca
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-05-01 19:34:43 +0530

    [Sanju] ADD :  Welcome to stack and queue.

---
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===============Welcome To Stack And Queue Program Using Linked List=====================");
+        }
+    }
+}
