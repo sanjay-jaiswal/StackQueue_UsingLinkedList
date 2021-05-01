eab73ca6eab429caa2054c36abbf39173c6efc96
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-05-01 19:50:16 +0530

    [Sanju] ADD : Pushing elements into stack.

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -7,6 +7,18 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("===============Welcome To Stack And Queue Program Using Linked List=====================");
+
+            Stack stackOBJ = new Stack();
+
+            //Pushing(adding) elements one by one.
+            //Top elements will be 50.
+            stackOBJ.Push(70);
+            stackOBJ.Push(30);
+            stackOBJ.Push(56);
+
+            //Displaying stack.
+            stackOBJ.DisplayStackElements();
+
         }
     }
 }
---
+++ StackAndQueue/StackAndQueue/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    /// <summary>
+    /// Stack class 
+    /// </summary>
+    public class Stack
+    {
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+
+        /// <summary>
+        /// Push elements into stack.
+        /// </summary>
+        /// <param name="value"></param>
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed into stack ", value);
+        }
+
+        /// <summary>
+        /// Display method to display stack elements.
+        /// </summary>
+        public void DisplayStackElements()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
