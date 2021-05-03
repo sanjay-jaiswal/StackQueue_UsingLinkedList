b93b5577f9352807c9b4eba68fc68aee1877c9f3
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-05-03 08:17:23 +0530

    [Sanju] ADD : Enqueue in queue.

---
+++ StackAndQueue/StackAndQueue/AbilityToEnQueueInQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+   public class AbilityToEnQueueInQueue
+    {
+        Node head = null;
+
+        /// <summary>
+        /// Inserting elements in queue -----> FIFO order.
+        /// </summary>
+        /// <param name="data"></param>
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into queue. ", node.data);
+        }
+
+        /// <summary>
+        /// Display queue elements.
+        /// </summary>
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty!!!");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write("Queue elements are : "+temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -9,6 +9,7 @@ namespace StackAndQueue
             Console.WriteLine("===============Welcome To Stack And Queue Program Using Linked List=====================");
 
             Stack stackOBJ = new Stack();
+            AbilityToEnQueueInQueue queueOBJ = new AbilityToEnQueueInQueue();
 
             //Pushing(adding) elements one by one.
             //Top elements will be 50.
@@ -24,6 +25,18 @@ namespace StackAndQueue
             stackOBJ.Pop();
             stackOBJ.IsEmpty();
             stackOBJ.DisplayStackElements();
+
+            //Queue operations
+            queueOBJ.Enqueue(44);
+            queueOBJ.Enqueue(440);
+            queueOBJ.Enqueue(344);
+            queueOBJ.Enqueue(333);
+            queueOBJ.Enqueue(12);
+            queueOBJ.Display();
+
+
+
+
 
         }
     }
